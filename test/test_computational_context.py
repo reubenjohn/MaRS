d68859b63a5ab13cee2a31764b14f584cc1b2f49
Reuben John <reubenvjohn@gmail.com>
2023-11-24 21:36:31 -0800

    Configured CI to generate test coverage

--- test/test_computational_context.py
+++ test/test_computational_context.py
@@ -1,6 +1,8 @@
 import sys
 from unittest import TestCase
 
+from mars.main import hello
+
 
 class Test(TestCase):
     def test_python_version(self):
@@ -8,3 +10,6 @@ class Test(TestCase):
         splits = python_version.split(".")
         self.assertEqual("3", splits[0])
         self.assertGreaterEqual(int(splits[1]), 7)
+
+    def test_hello(self):
+        self.assertEqual(hello(), 1)
